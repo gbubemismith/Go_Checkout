d0a22fb4fb0d11a21d3d65b91f0520909baa841f
gbubemismith <gsmithwalter@gmail.com>
2021-10-23 00:16:56 +0100

    serving html pages

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,5 +3,8 @@ package main
 import "net/http"
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	app.infoLog.Println("Hit the handler")
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+	}
+
 }
