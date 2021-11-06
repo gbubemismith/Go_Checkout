2c406e032a739f16c192b2b046bd1ad6f477e76c
gbubemismith <gsmithwalter@gmail.com>
2021-11-06 23:57:51 +0100

    added update

--- cmd/API/routes.api.go
+++ cmd/API/routes.api.go
@@ -17,7 +17,7 @@ func (app *application) routes() http.Handler {
 		MaxAge:         300,
 	}))
 
-	mux.Get("/api/payment-intent", app.GetPaymentIntent)
+	mux.Post("/api/payment-intent", app.GetPaymentIntent)
 
 	return mux
 }
